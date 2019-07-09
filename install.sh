bc8f01da363d00bf3dd13505852fd14b3b53a46d
Jaydeep <jaydeep.hasmukhlal@academytrainee.com>
2019-07-09 01:07:18 -0700

    Created install and service file.

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CYAN="\033[0;36"
+GREEN="\033[0;32m"
+DEFULT="\e[39mDefault"
+BLINK="\e[5mBlink"
+NOBLINK="\e[25mNormal"
+
+echo -e "${BLINK}${CYAN}Updateing."
+sudo apt-get update -y && apt-get upgrade -y
+
+sudo apt-get install curl python-software-properties
+sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+echo -e "${BLINK}${GREEN} Finished updating."
+
+echo -e "${BLINK}${CYAN} Installing Git, NodeJs."
+sudo apt-get install -y git
+sudo apt-get install -y nodejs
+echo -e "${BLINK}${GREEN} Finished Installing."
+
+echo -e "${BLINK}${CYAN} Installing API Service."
+sudo cp ./poolui.service /etc/systemd/system/
+sudo systemctl daemon-reload
+echo -e "${BLINK}${GREEN}Finished installing API Service."
+
+echo -e "${BLINK}${CYAN}Cloning GIT repo."
+sudo git clone https://github.com/Nboaram/TeamAPoolProjectUI.git
+
+echo -e "${BLINK}${CYAN}Moving project to base."
+sudo mv TeamAPoolProjectUI/ ../TeamAPoolProjectUI/
+
+echo -e "${BLINK}${CYAN}CD into project."
+sudo cd ../TeamAPoolProjectUI
+
+echo -e "${BLINK}${CYAN}Checking into Developer."
+sudo git checkout Developer
+
+echo -e "${BLINK}${CYAN}Installing angular and dependencies."
+sudo npm install -g @angular/cli
+sudo npm install
